b958c4f8a21c8926b875454bb034a06e5d3bd44a
Kim Arre <Kiwiiixo@gmail.com>
2013-07-30 19:20:17 -0700

    pseudocode for problem 2

---
+++ 002.py
@@ -0,0 +1,6 @@
+#sum the values of the fibbonacci sequence that are even
+#while var1 isn't larger than 4 million
+#var1 = the current value, var2 = last value
+# if val1 is even, add it to sum,
+# else, store var2 in temp and add the two.
+# then set` result to var2 

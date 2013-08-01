ce3e11269b13b25c763a1b5fa1c9b4553e03bc8a
Kim Arre <Kiwiiixo@gmail.com>
2013-07-31 20:51:36 -0700

    Finished problem 3

--- 003.py
+++ 003.py
@@ -1,5 +1,31 @@
 #Problem 3
 
+'''Pseudocode:
+for values 2-10 
+    if the values are divisible by i
+        num = num/i
+
+'''
+currentNum = 600851475143
+half = 600851475143/2
+highest = 0
+
+for i in xrange(2, half):
+   if currentNum % i == 0:
+      currentNum = currentNum/i
+      highest = i
+   if currentNum <= 1:
+      break
+
+if currentNum > highest:
+   print currentNum
+else:
+   print highest
 
 
+'''highRange = 600851475143/2
+
+for i in range (2,highRange):
+   if 
+'''
 
